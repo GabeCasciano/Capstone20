61723ceff9d9580514af816c06215037d2089669
ndshetty <ndshetty22@gmail.com>
2021-02-23 21:41:55 -0500

    Segmentation complete
    spits out segment values for each distance point

--- Obstacle_Det/main.py
+++ Obstacle_Det/main.py
@@ -2,7 +2,6 @@ from rplidar import RPLidar
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
-import statistics
 
 PORT_NAME = '/COM3'
 IMIN = 0
@@ -12,14 +11,13 @@ IMAX = 50
 def update_line(num, iterator, line):
     scan = next(iterator)
     offsets = np.array([(np.radians(point[1]), point[2]) for point in scan])
-    # print(offsets.shape)
     x = range_filter(offsets, 0, 1000)
-    x = np.asarray(x)
-    x = np.transpose(x)
-    # print(x.shape)
+    x = np.asarray(x)  # saves array as rows
+    x = np.transpose(x)  # transpose to col
     offsets[:, 1] = x
-    # offsets = temp_median_filter(offsets, 2000)
-    line.set_offsets(offsets)
+    offsets = offsets[np.all(offsets != 0, axis=1)]  # removes rows with 0s
+    thres = distance_threshold(offsets, 20) # distance threshold function
+    line.set_offsets(thres)
     intens = np.array([point[0] for point in scan])
     line.set_array(intens)
     return line,
@@ -27,8 +25,6 @@ def update_line(num, iterator, line):
 
 def range_filter(scans, dmin, dmax):
     # assuming we have to define max and min from distance values- so 0 to 2500?
-    # line =
-    # for point in scans:
     x = scans[:, 1]
     for i in range(len(x)):
         if x[i] < dmin:
@@ -38,46 +34,41 @@ def range_filter(scans, dmin, dmax):
     return x
 
 
-# return [[min(max(point[1], dmin), dmax) for point in scans]]  # found this way, tryna implemenet something else tho
-
-
-#
-#
-# def temp_median_filter(scans, d):
-#     result = []
-#     for current in range(len(scans)):
-#         medians = []
-#         try:
-#             for previousIndex in range(len(scans[current])):
-#                 # change this from statistics to angle and distance?
-#                 medians.append(statistics.median([scan[previousIndex]
-#                                                   for scan in scans[max(-1, current - d) + 1: current + 1]]))
-#         except IndexError as err:
-#             print(err)
-#         #             #exit()  # comment this line if u still want the values with varied scans
-#         result.append(medians)
-#     return result
+def distance_threshold(scans, seg_threshold):
+    i = 1  # incremental num
+    temp = scans[:, 1]
+    thres = np.zeros((len(temp), 3))
+    thres[:, 0] = temp
+    x = [temp[len(temp) - 1]]
+    np.asarray(x)
+    temp = np.append(temp, x, axis=0)
+    thres[:, 1] = abs(np.diff(temp, axis=0))
+    cond_1 = thres[:, 1] > seg_threshold    # conditions where segment threshold > 20 mm, can be changed
+    check = np.where(cond_1, 2, 1)
+    check = check.reshape(-1, 1)
+    iter_seg = 1
+    for k in range(len(check)):
+        if check[k] == 2:
+            iter_seg = iter_seg + 1
+            check[k] = iter_seg
+        elif check[k] == 1:
+            check[k] = iter_seg
+    thres[:, 2] = check[:, 0]
+    return thres
 
 
 def run():
     lidar = RPLidar(PORT_NAME)
     dmin = 0
-    dmax = 2500
+    dmax = 1500
 
-    # d = 3
-    #
-    # # calling functions and error handling for varied scan lengths
-    # filter1_value = range_filter(update_line, dmin, dmax)
-    # filter2_value = list(temp_median_filter(filter1_value, d + 1))
-    #
-    # print("input", update_line, "\n""after_MixMax_filter", filter1_value, "\n""After_temp_median:", filter2_value)
     fig = plt.figure()
     ax = plt.subplot(111, projection='polar')
     line = ax.scatter([0, 0], [0, 0], s=5, c=[IMIN, IMAX],
                       cmap=plt.cm.Greys_r, lw=0)
+    ax.set_rmax(dmin)
     ax.set_rmax(dmax)
     ax.grid(True)
-
     iterator = lidar.iter_scans()
     ani = animation.FuncAnimation(fig, update_line,
                                   fargs=(iterator, line), interval=50)
@@ -88,3 +79,5 @@ def run():
 
 if __name__ == '__main__':
     run()
+
+
